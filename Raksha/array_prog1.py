6ad74a120c63fa8e6f28f70bf9321bb43b505958
ComputerGeekNerd <rakshaaasish@gmail.com>
2019-03-06 06:29:50 +0530

    Initial commit

---
+++ Raksha/array_prog1.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+
+def minmax():
+    l = (int(x) for x in input("Enter numbers: ").split())
+
+
+    min = l[0]
+    max = l[0]
+
+    for i in l:
+        if i < min:
+            min = i
+        if i > max:
+            max = i
+
+    print("Min = ",min)
+    print("Max = ",max)
+
+minmax()
